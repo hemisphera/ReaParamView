c91be47c70b9246fd84897f5d55f4f8f682eb8b8
The Dumb <info@hemisphera.com>
2026-04-02 15:20:37 +0200

    Layout fixes

--- ReaParamView.WebApp/Program.cs
+++ ReaParamView.WebApp/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Http.Features;
 using ReaParamView.WebApp.Components;
 using ReaParamView.WebApp.Services;
 
@@ -29,4 +32,34 @@ app.MapStaticAssets();
 app.MapRazorComponents<App>()
   .AddInteractiveServerRenderMode();
 
-app.Run();
+await app.StartAsync();
+
+// Output the addresses the app is listening on
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+var server = app.Services.GetRequiredService<IServer>();
+var addressesFeature = server.Features.GetRequiredFeature<IServerAddressesFeature>();
+
+// Get local IP address for network access
+var hostName = System.Net.Dns.GetHostName();
+var ips = System.Net.Dns.GetHostAddresses(hostName);
+var localIp = ips.FirstOrDefault(ip => 
+  ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork && 
+  !ip.ToString().StartsWith("127."));
+
+if (localIp != null && addressesFeature.Addresses.Count > 0)
+{
+  if (Uri.TryCreate(addressesFeature.Addresses.First(), UriKind.Absolute, out var uri))
+  {
+    var endpoint = $"http://{localIp}:{uri.Port}";
+    logger.LogInformation("WebApp is running at: {Endpoint}", endpoint);
+  }
+}
+else
+{
+  foreach (var address in addressesFeature.Addresses)
+  {
+    logger.LogInformation("WebApp is running at: {Address}", address);
+  }
+}
+
+await app.WaitForShutdownAsync();
